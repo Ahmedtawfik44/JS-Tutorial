b37799a214a7e6ad783da7a2ac11a9177e52033c
Ahmedtawfik44 <ahmedtawfiek556@gmail.com>
2025-02-21 20:35:50 +0200

    Challange Concatination

---
+++ challange/main.js
@@ -0,0 +1,11 @@
+let the_Title="Elzero",the_Description="Elzero web school",the_Date="25/10";
+
+let cart=`
+
+<div class="cart">
+    <h3>Hellow ${the_Title}</h3>
+    <p>${the_Description}</p>
+    <span>${the_Date}</span>
+</div>`;
+
+document.write(cart.repeat(4));
