2b02ead155d2b3b1372ff61ecabcfb43a585f206
Ahmedtawfik44 <ahmedtawfiek556@gmail.com>
2025-03-15 00:19:28 +0200

    Arrow Function Challenge

---
+++ challange arrow fn/main.js
@@ -0,0 +1,29 @@
+/*
+  Function Arrow Challenges
+*/
+
+// [1] One Statement In Function
+// [2] Convert To Arrow Function
+// [3] Print The Output [Arguments May Change]
+
+let names = (...data) => `String [${data.join("],[")}] =>Done`
+
+console.log(names("Osama", "Mohamed", "Ali", "Ibrahim"));
+// String [Osama], [Mohamed], [Ali], [Ibrahim] => Done !
+
+/* ================================= */
+
+// [1] Replace ??? In Return Statement To Get The Output
+// [2] Create The Same Function With Regular Syntax
+// [3] Use Array Inside The Arguments To Get The Output
+
+  let myNumbers = [20, 50, 10, 60];
+
+//   let calc = (one, two, ...nums) => one + two + nums[+!Array.isArray(myNumbers)];
+ 
+ function calc(one, two, ...nums){
+    return one + two + nums[+!Array.isArray(myNumbers)];
+ }
+
+
+  console.log(calc(10, myNumbers[2], myNumbers[3])); // 80
